912725d65d6e351d8a742bc02e2fb77c2ca4eb88
Martin Quintana <marquiran@freimartnsarmiento.com>
2022-09-22 14:30:56 +0200

    22/09/22

---
+++ UD1 Acceso a ficheiros/Ejercicios y pruebas/CrearyBorrarFichero/src/EjemploCrearBorrarFichero.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Scanner;
+
+public class EjemploCrearBorrarFichero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nombre = sc.nextLine();
+        Path fichero = Paths.get(nombre);
+        // Files.exists(fichero)
+        try {
+            try {
+                Files.createFile(fichero);
+                System.out.println("Se ha creado el fichero");
+            } catch (FileAlreadyExistsException ex) {
+                System.out.println("El fichero ya existía");
+            }
+
+
+            Path fichero = Paths.get()
+            System.out.println("Ruta absoluta: " + fichero.toAbsolutePath());
+            System.out.print("Quieres borrar el fichero " + fichero + "(s/N)? ");
+            if (sc.nextLine().equalsIgnoreCase("s")) {
+                Files.delete(fichero);
+                System.out.println("Se ha borrado el fichero.");
+            } else {
+                System.out.println("Se ha conservado el fichero.");
+            }
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+
+}
