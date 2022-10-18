232b81f1b407427dc6a6252550256a65c1e4c1bc
martinuxx18 <martinquintanarana@gmail.com>
2022-10-18 10:38:14 +0200

    18/10/22

--- UD1 Acceso a ficheiros/Entregas/Tarefa EA.1.2. Iniciacion Ficheros/Ejer1/src/Ejercicio1.java
+++ UD1 Acceso a ficheiros/Entregas/Tarefa EA.1.2. Iniciacion Ficheros/Ejer1/src/Ejercicio1.java
@@ -6,38 +6,38 @@ import java.util.Scanner;
  * Programa que reciba el nombre de un directorio y muestre su contenido y si se trata de un fichero o un directorio.
  */
 public class Ejercicio1 {
-	public static void main(String[] args) {
-		Path carpetaALeer;
+    public static void main(String[] args) {
+        Path carpetaALeer;
 
 		/*
 			El directorio a listar se intenta obtener por parámetros, pero si hay más de
 			uno o no hay ninguno, se pide por consola.
 		 */
-		if (args.length == 1) {
-			carpetaALeer = Path.of(args[0]);
-		} else {
-			var teclado = new Scanner(System.in);
-			System.out.println("¿Qué directorio desea listar? ");
-			carpetaALeer = Path.of(teclado.nextLine());
-		}
+        if (args.length == 1) {
+            carpetaALeer = Path.of(args[0]);
+        } else {
+            var teclado = new Scanner(System.in);
+            System.out.println("¿Qué directorio desea listar? ");
+            carpetaALeer = Path.of(teclado.nextLine());
+        }
 
-		if (!Files.exists(carpetaALeer) || !Files.isDirectory(carpetaALeer)) {
-			System.err.println("La ruta especificada no es un directorio o no existe");
-			System.exit(1);
-		}
+        if (!Files.exists(carpetaALeer) || !Files.isDirectory(carpetaALeer)) {
+            System.err.println("La ruta especificada no es un directorio o no existe");
+            System.exit(1);
+        }
 
-		try (var ds = Files.newDirectoryStream(carpetaALeer)) {
-			ds.forEach((elemento) -> {
-				String sb = (Files.isDirectory(elemento) ? "d" : "-") +
-						(Files.isReadable(elemento) ? "r" : "-") +
-						(Files.isWritable(elemento) ? "w" : "-") +
-						(Files.isExecutable(elemento) ? "x" : "-") +
-						"\t" +
-						elemento.getFileName();
-				System.out.println(sb);
-			});
-		} catch (Exception ex) {
-			System.err.println(ex.getMessage());
-		}
-	}
+        try (var ds = Files.newDirectoryStream(carpetaALeer)) {
+            ds.forEach((elemento) -> {
+                String sb = (Files.isDirectory(elemento) ? "d" : "-") +
+                        (Files.isReadable(elemento) ? "r" : "-") +
+                        (Files.isWritable(elemento) ? "w" : "-") +
+                        (Files.isExecutable(elemento) ? "x" : "-") +
+                        "\t" +
+                        elemento.getFileName();
+                System.out.println(sb);
+            });
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
 }
