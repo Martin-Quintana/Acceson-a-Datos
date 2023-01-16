9897083dc37f19ce1a3a70c0219d809e6529c2ad
Martin Quintana <martinquintanarana@gmail.com>
2023-01-16 13:36:17 +0100

    16/1/23

--- UD4 SprinBoot/demoapp-master/src/main/java/demoapp/controller/SaludoControllerForm.java
+++ UD4 SprinBoot/demoapp-master/src/main/java/demoapp/controller/SaludoControllerForm.java
@@ -29,7 +29,19 @@ public class SaludoControllerForm {
         if (bindingResult.hasErrors()) {
             return "formRegistro";
         }
+        //El model se inicializa y esta vacio hasta que se le añaden atributos.
+
+
+        //Al model le añadimos dos atributos mensaje y edad para que se guarden cuando sea introducidos desde el formulario
+        // y el usuario le de al boton de enviar.
+
+        //Eek formRegistro cuando se le da al boton de enviar, se llama automaticamente a saludo.html y te pone tu edad
+        // y el nombre que le pusiste en el formulario.
+
+        //Model 'mensaje'
         model.addAttribute("mensaje", service.saluda(userData.getNombre()));
+
+        //Model 'edad'
         model.addAttribute("edad", service.edad(userData.getEdad().toString()));
         return "saludo";
     }
--- UD4 SprinBoot/demoapp-master/src/main/java/demoapp/service/SaludoService.java
+++ UD4 SprinBoot/demoapp-master/src/main/java/demoapp/service/SaludoService.java
@@ -4,9 +4,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SaludoService {
+
+    //Manda un saludo en la clase saludo.html
     public String saluda(String nombre) {
         return "Hola " + nombre;
     }
+
+    //Dice que edad tienes la cual se ha introducido desde el formulario
     public String edad(String edad) { return " tienes " + edad + " anhos";
     }
 
